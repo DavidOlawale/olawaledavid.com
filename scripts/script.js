f7eea0a09c01db33c080cfd73ca3bb3486f47e95
davidolawale <olawaledavid11@gmail.com>
2020-05-28 21:59:42 +0100

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+var options = {
+    strings: ['Front end developer', 'Back end developer', 'Full stack end developer'],
+    typeSpeed: 40
+  };
+  
+  var typed = new Typed('.typed', options)
+
+  AOS.init({
+    duration: 1000
+  })
